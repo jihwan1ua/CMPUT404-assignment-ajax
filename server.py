26438a6f41eb6f26166c18adf273cc735a2278cd
jihwan <jihwan@ualberta.ca>
2016-10-24 14:47:40 -0600

    server done, i think

--- server.py
+++ server.py
@@ -22,7 +22,12 @@
 
 
 import flask
-from flask import Flask, request
+'''
+additional import: redirect, jsonify
+redirect for redirecting url, jsonify for passing dict object from server to client
+'''
+
+from flask import Flask, request, redirect, jsonify
 import json
 app = Flask(__name__)
 app.debug = True
@@ -42,15 +47,19 @@ class World:
         entry[key] = value
         self.space[entity] = entry
 
+    # set some 
     def set(self, entity, data):
         self.space[entity] = data
 
+    # clear seems to be clearing the world
     def clear(self):
         self.space = dict()
 
+    # need to use this get to get entities from server then pass it to client
     def get(self, entity):
         return self.space.get(entity,dict())
-    
+
+    # this only returns world excluding entities.
     def world(self):
         return self.space
 
@@ -71,30 +80,71 @@ def flask_post_json():
     else:
         return json.loads(request.form.keys()[0])
 
+'''
+Note to self.
+It is very important that I should return json object to client once
+I modify the World object, if I do not return new json with modified World,
+the client will not acknowledge the changes made.
+Also retrieve json from client using flask_post_json method.
+'''
+
 @app.route("/")
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return redirect("/static/index.html"), 301
 
+# we dont return myWorld.world() here since we only using entity
+# so we need to get entity then return that to json
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    client_json = flask_post_json()
+    if request.method == 'POST':
+        # if method is POST, then we set the entity
+        myWorld.set(entity, client_json)
+        # now need to get the entities from world
+        ent = myWorld.get(entity)
+        return flask.jsonify(**ent)
 
+    elif request.method == 'PUT':
+        # if method is PUT, then we update the entity
+        for key in client_json.keys():
+            myWorld.update(entity, key, client_json[key])
+        # now need to get the entities from world
+        ent = myWorld.get(entity)
+        return flask.jsonify(**ent)
+
+# we can return myWorld here since no entities were modified
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    if request.method == 'POST':
+        # if method is POST, then we set the world, but entity not given...
+        client_json = flask_post_json()
+        myWorld.set(client_json)
+        return flask.jsonify(myWorld.world())
+    elif request.method == 'GET':
+        # if method is GET, then we return the world
+        return flask.jsonify(myWorld.world())
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    # we read the entity
+    ent = myWorld.get(entity)
+    return flask.jsonify(**ent)
 
+# we can return myWorlds here since no entities were modified
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    if request.method == 'POST':
+        myWorld.clear()
+        return flask.jsonify(myWorld.world())
+
+    elif request.method == 'GET':
+        myWorld.clear()
+        return flask.jsonify(myWorld.world())
 
 if __name__ == "__main__":
     app.run()
